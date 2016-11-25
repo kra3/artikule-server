c58422666884ed395cf60af296a8c959de997c07
Arun Karunagath (kra3) <the1.arun@gmail.com>
2016-11-25 18:54:43 +0530

    admin has to show every articles

--- articles/admin.py
+++ articles/admin.py
@@ -10,3 +10,10 @@ class ArticleAdmin(admin.ModelAdmin):
     list_filter = ('author', 'category', 'publication_date')
     save_as = True
     search_fields = ('title',)
+
+    def get_queryset (self, request):
+        qs = self.model._default_manager.every_article()
+        ordering = self.get_ordering(request)
+        if ordering:
+            qs = qs.order_by(*ordering)
+        return qs
