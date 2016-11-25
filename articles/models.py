9ac6f7ac49c0413222dd101dec22be9898c394f1
Arun Karunagath (kra3) <the1.arun@gmail.com>
2016-11-25 19:13:58 +0530

    Added custom methods to model

--- articles/models.py
+++ articles/models.py
@@ -1,9 +1,12 @@
+from django.core.paginator import Paginator
 from django.db import models
 from easy_thumbnails.fields import ThumbnailerImageField
 from .managers import ArticleManager
 
 
 class Article(models.Model):
+    SUGGESTIONS_COUNT = 4
+
     title = models.CharField(max_length=128)
     # at a later point, we could associate current logged in user as author.
     author = models.CharField(max_length=64)
@@ -20,3 +23,21 @@ class Article(models.Model):
 
     def __str__(self):
         return "<Article: {} - {}>".format(self.title, self.author)
+
+    @classmethod
+    def get_random_article(cls):
+        return cls.objects.random_article()
+
+    @classmethod
+    def get_article_suggestions(cls, current_page=0):
+        # I'm using a simple pagination over all the avialble articles
+        # however in real life, we have to look for multiple scenarios
+        # - avoid showing the same articles which is shown in the
+        # same page in other sections.
+        # - implement as a recomenndation system where we have to show
+        # similar articles to currently displayed articles, also user's
+        # taste will come into play.
+        paginator = Paginator(cls.objects.all(), cls.SUGGESTIONS_COUNT)
+        next_page = current_page + 1
+        next_page = next_page if next_page in paginator.page_range else 1
+        return paginator.page(next_page)
