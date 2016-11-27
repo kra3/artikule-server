cea24a1a52511a29fbc99377a6acb4a311ee6c95
Arun Karunagath (kra3) <the1.arun@gmail.com>
2016-11-27 15:26:01 +0530

    bug fix with pagination

--- articles/views.py
+++ articles/views.py
@@ -9,17 +9,20 @@ from .models import Article
 
 @require_safe
 def what_to_read_next(request):
-    next_page = request.GET.get('next_page', 1)
+    next_page = request.GET.get('next_page', '1')
+    next_page = int(next_page) if next_page.isdigit() else 1
     page = Article.get_article_suggestions(next_page)
     page['articles'] = [article.as_json for article in page['articles']]
     return JsonResponse(page)
 
 
+@require_safe
 def featured_article(request):
     featured = Article.get_random_article()
     return JsonResponse(featured.as_json)
 
 
+@require_safe
 def articles(request):
     articles = Article.objects.all()
     return JsonResponse(dict(articles=[article.as_json for article in articles]))
